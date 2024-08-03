dc6d87a5664ed5815293bc45ae70d298efdc9dfe
AkshayKothule <103442517+AkshayKothule@users.noreply.github.com>
2024-08-03 05:42:30 +0000

    commit

---
+++ Basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId=43555;
+let accountEmail="akshay@gogle.com"
+var accountPassword="45555"
+accountCity="pune"
+// accountId=5;
+accountEmail="kothule@gogle.com"
+accountPassword="12345"
+accountCity="Mumbai"
+
+console.log(accountId);
+console.table([accountId,accountEmail ,accountPassword ,accountCity])
