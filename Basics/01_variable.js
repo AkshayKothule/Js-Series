95bc5a07c26e2c87c6a09c6d77786600578a8d1d
AkshayKothule <103442517+AkshayKothule@users.noreply.github.com>
2024-08-04 07:47:50 +0000

    datatype

--- Basics/01_variable.js
+++ Basics/01_variable.js
@@ -1,7 +1,10 @@
+
+
 const accountId=43555;
 let accountEmail="akshay@gogle.com"
 var accountPassword="45555"
 accountCity="pune"
+
 // accountId=5;
 accountEmail="kothule@gogle.com"
 accountPassword="12345"
@@ -9,3 +12,11 @@ accountCity="Mumbai"
 
 console.log(accountId);
 console.table([accountId,accountEmail ,accountPassword ,accountCity])
+
+/*"var" is the oldest way to declare a variable in JavaScript. Variables declared with "var" have a function-level scope, meaning they are only accessible within the function in which they were declared*/
+/*
+"let" have the block-level scope, meaning they are only accessible within the block in which they were declared */
+
+/*"const"  is used to declare variables that cannot be reassigned. This makes "const" variables useful for declaring constants, such as pi or the gravitational constant,*/
+
+/*"let", "const" variables have the block-level scope, and, they are not accessible before they are declared.*/
---
+++ Basics/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict" //treat all js code as newer version
+
+// alert(3+3) //we are using nodejs , not browser
+
+// console.log(3+3); 
+
+let name="akshay";
+let age=22;
+let isLogdin=false;
+
+//datatype in js 
+
+//number :- 2 to power 53
+//string
+//boolean ;- there are true or false 
+//null :-standalone value
+//undefined :- we deaclear varaibel but cannot assined value 
+
+// console.log(typeof name);
+console.log(typeof null); //output :- object 
+console.log(typeof undefined);//output :- undefined
