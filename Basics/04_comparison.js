542948c8760a9d06ee0bccf303caa7d18942493d
AkshayKothule <103442517+AkshayKothule@users.noreply.github.com>
2024-08-05 04:33:58 +0000

    comparision

---
+++ Basics/04_comparison.js
@@ -0,0 +1,16 @@
+
+// console.log(2 >1);
+// console.log(2 ==1 );
+// console.log(2 >=1);
+// console.log(2 < 1);
+// console.log(2 <= 1)
+
+// console.log("2" > 1); //convert strnginto number
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+
