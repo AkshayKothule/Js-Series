7b9508d16729da368aefce3dfa177d042591e32f
AkshayKothule <103442517+AkshayKothule@users.noreply.github.com>
2024-08-05 04:13:52 +0000

    datatype coversion

---
+++ Basics/03_conversionOpration.js
@@ -0,0 +1,80 @@
+// let score="33abc";
+// let score=null;
+// let score=undefined;
+// let score=false;
+let score="Akshay";
+
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber=Number(score);
+// console.log(typeof valueInNumber );
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//true => 1 ; false=>0
+
+let isLogdin="Akshay";
+
+let booleanIsLogdin=Boolean(isLogdin);
+// console.log(booleanIsLogdin);
+
+//1 => true ; 0 => false
+//""=> false
+//"Akshay"=> true
+
+let marks=55;
+
+let stringmarks=String(marks);
+// console.log(stringmarks);
+// console.log(typeof stringmarks);
+
+
+/* ***********************Oprations**************** 
+*/
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str="Akshay";
+let str1="Kothule"
+
+let str2=str + str1;
+// console.log(str2);
+let value="1"+2;
+// console.log(value);
+// console.log(typeof value);
+let value1=1+1+"2";
+// console.log(value1);
+// console.log(typeof value1);
+
+// console.log("2"+"2"+1);
+// console.log(10+10+1+"5");
+
+// If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+
+// If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+
+
+
+
+
+let x=5;
+let y=++x;
+console.log(`x:${x} , y:${y}`); // 6
+
+let a=10;
+let b=a++;
+console.log(`a:${a} , b:${b}`); // 6
+
+
+
+
+
