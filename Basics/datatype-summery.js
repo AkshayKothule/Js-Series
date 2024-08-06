68e625068de1919ebd3a526100aeddbcfd887c01
AkshayKothule <103442517+AkshayKothule@users.noreply.github.com>
2024-08-06 06:19:33 +0000

    datatype-summery

---
+++ Basics/datatype-summery.js
@@ -0,0 +1,33 @@
+// primitive datatype
+
+// 7 type : string , number , boolean , null , undefined ,symbol , BigInt
+
+const name="akshay";
+let temp=45;
+let userid;
+
+const id=Symbol('123');
+const anotherid=Symbol('123');
+console.log(id===anotherid);
+
+// JavaScript is a dynamic language and not static, which means that variables can hold values of different types during runtime
+
+//reference type or non-primitive
+// Array , objects , functions
+
+const heros=["shaktiman" , "doga" , "ironman"];
+
+// this object and they have decleard key and value
+let myobj={
+    name:"akshay",
+    age:22
+}
+
+const myfunction=function(){
+    console.log("hello word");
+    
+}
+
+// link
+
+// https://ecma-international.org/publications-and-standards/standards/ecma-262/
