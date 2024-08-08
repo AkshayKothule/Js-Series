d5278f64d2a89ad6c60fe8e78bcf8646157042ac
AkshayKothule <103442517+AkshayKothule@users.noreply.github.com>
2024-08-08 15:29:09 +0000

    date and Time

---
+++ Basics/07_dateinJs.js
@@ -0,0 +1,27 @@
+let mydate=new Date();
+// console.log(mydate);
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toString());
+// console.log(mydate.toLocaleString());
+
+// console.log(mydate.toTimeString());
+
+// console.log(mydate.toISOString());
+
+
+let newDate=new Date();
+// console.log(newDate.getDate());
+// console.log(newDate.getFullYear());
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getSeconds());
+
+newDate.toLocaleString('default', {
+    weekday:"long",
+
+})
+
+
+
+
+
