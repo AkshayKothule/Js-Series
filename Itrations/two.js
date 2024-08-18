fe560baaf3d58c93295a3fcd9b5b3cdfc8326abc
AkshayKothule <103442517+AkshayKothule@users.noreply.github.com>
2024-08-18 10:04:10 +0000

    while loop

---
+++ Itrations/two.js
@@ -0,0 +1,30 @@
+
+let index=0;
+
+// while( index <=10)
+// {
+//     console.log(`value of index ${index}`);
+//     index=index+2;
+    
+// }
+
+
+let myArray=["batman" , "ironMan" , "Thor " , "Captain-america"]
+
+let i=0 ;
+// while( i< myArray.length)
+// {
+//     console.log(myArray[i]);
+//     i=i+1;
+    
+// }
+
+
+let score=11;
+
+do{
+    console.log(`score is ${score}`);
+    score++;
+    
+
+}while(score <= 10)
