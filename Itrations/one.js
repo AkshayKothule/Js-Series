9f53f8a140e91ba14ff5e489889fe7d677b6ceda
AkshayKothule <103442517+AkshayKothule@users.noreply.github.com>
2024-08-18 09:56:10 +0000

    for loop

---
+++ Itrations/one.js
@@ -0,0 +1,50 @@
+
+// for
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+
+//     if(element==5)
+//     {
+//         console.log("5  is best number");
+        
+//     }
+//     console.log(element);
+    
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`outer loop ${i}`);
+    
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`inner loop${j}`);   
+//     }    
+// }
+
+
+
+// let myArray=["flash", "batman" , "ironMan"]
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+    
+// }
+
+//break and continue
+
+for (let i = 0; i < 10; i++) {
+    
+    if(i==5)
+    {
+        console.log(`5 is best in number`);
+        break;
+        // continue;
+        
+    }
+    console.log(i);
+
+    
+    
+}
